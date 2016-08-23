5869236599e8462f68594ad986c219de7a270ab0
achernyak <artemchernyak@gmail.com>
2016-08-23 08:27:28 -0500

    dod human vs human

--- dice-of-doom/dice-of-doom.lisp
+++ dice-of-doom/dice-of-doom.lisp
@@ -80,3 +80,60 @@
 		  collect (cond ((eq pos src) (list player 1))
 				((eq pos dst) (list player (1- dice)))
 				(t hex)))))
+
+(defun add-new-dice (board player spare-dice)
+  (labels ((f (lst n)
+	     (cond ((null lst) nil)
+		   ((zerop n) lst)
+		   (t (let ((cur-player (caar lst))
+			    (cur-dice (cadar lst)))
+			(if (and (eq cur-player player) (< cur-dice *max-dice*))
+			    (cons (list cur-player (1+ cur-dice))
+				  (f (cdr lst) (1- n)))
+			    (cons (car lst) (f (cdr lst) n))))))))
+    (board-array (f (coerce board 'list) spare-dice))))
+
+(defun play-vs-human (tree)
+  (print-info tree)
+  (if (caddr tree)
+      (play-vs-human (handle-human tree))
+      (announce-winner (cadr tree))))
+
+(defun print-info (tree)
+  (fresh-line)
+  (format t "current player = ~a" (player-letter (car tree)))
+  (draw-board (cadr tree)))
+
+(defun handle-human (tree)
+  (fresh-line)
+  (princ "chose your move:")
+  (let ((moves (caddr tree)))
+    (loop for move in moves
+       for n from 1
+       do (let ((action (car move)))
+	    (fresh-line)
+	    (format t "~a. " n)
+	    (if action
+		(format t "~a -> ~a" (car action) (cadr action))
+		(princ "end turn"))))
+    (fresh-line)
+    (cadr (nth (1- (read)) moves))))
+
+(defun winners (board)
+  (let* ((tally (loop for hex across board
+		   collect (car hex)))
+	 (totals (mapcar (lambda (player)
+			  (cons player (count player tally)))
+			(remove-duplicates tally)))
+	 (best (apply #'max (mapcar #'cdr totals))))
+    (mapcar #'car
+	    (remove-if (lambda (x)
+			 (not (eq (cdr x) best)))
+		       totals))))
+
+(defun announce-winner (board)
+  (fresh-line)
+  (let ((w (winners board)))
+    (if (> (length w) 1)
+	(format t "The game is a tie between ~a" (mapcar #'player-letter w))
+	(format t "The winner is ~a" (player-letter (car w))))))
