1b951893721386e8aa0b4238ce06f28358105a1c
achernyak <artemchernyak@gmail.com>
2016-08-30 08:18:41 -0500

    lazy dice vs human

--- dice-of-doom/dice-of-doom.lisp
+++ dice-of-doom/dice-of-doom.lisp
@@ -36,7 +36,7 @@
 (defun add-passing-move (board player spare-dice first-move moves)
   (if first-move
       moves
-      (cons (list nil
+      (lazy-cons (list nil
 		  (game-tree (add-new-dice board player (1- spare-dice))
 			     (mod (1+ player) *num-players*)
 			     0
@@ -48,20 +48,26 @@
 	     (car (aref board pos)))
 	   (dice (pos)
 	     (cadr (aref board pos))))
-    (mapcan (lambda (src)
-	      (when (eq (player src) cur-player)
-		(mapcan (lambda (dst)
-			  (when (and (not (eq (player dst) cur-player))
+    (lazy-mapcan (lambda (src)
+	      (if (eq (player src) cur-player)
+		(lazy-mapcan (lambda (dst)
+			  (if (and (not (eq (player dst) cur-player))
 				     (> (dice src) (dice dst)))
-			    (list
-			     (list (list src dst)
-				   (game-tree (board-attack board cur-player src dst (dice src))
-					      cur-player
-					      (+ spare-dice (dice dst))
-					      nil)))))
-			(neighbors src))))
-	    (loop for n below *board-hexnum*
-	       collect n))))
+			    (make-lazy
+			     (list (list (list src dst)
+					 (game-tree (board-attack board
+								  cur-player
+								  src
+								  dst
+								  (dice src))
+						    cur-player
+						    (+ spare-dice (dice dst))
+						    nil))))
+			    (lazy-nil)))
+			     (make-lazy (neighbors src)))
+		(lazy-nil)))
+		 (make-lazy (loop for n below *board-hexnum*
+				  collect n)))))
 
 (defun neighbors (pos)
   (let ((up (- pos *board-size*))
@@ -97,7 +103,7 @@
 
 (defun play-vs-human (tree)
   (print-info tree)
-  (if (caddr tree)
+  (if (not (lazy-null (caddr tree)))
       (play-vs-human (handle-human tree))
       (announce-winner (cadr tree))))
 
@@ -108,18 +114,21 @@
 
 (defun handle-human (tree)
   (fresh-line)
-  (princ "chose your move:")
+  (princ "choose your move:")
   (let ((moves (caddr tree)))
-    (loop for move in moves
-       for n from 1
-       do (let ((action (car move)))
-	    (fresh-line)
-	    (format t "~a. " n)
-	    (if action
-		(format t "~a -> ~a" (car action) (cadr action))
-		(princ "end turn"))))
+    (labels ((print-moves (moves n)
+	       (unless (lazy-null moves)
+		 (let* ((move (lazy-car moves))
+			(action (car move)))
+		   (fresh-line)
+		   (format t "~a. " n)
+		   (if action
+		       (format t "~a -> ~a" (car action) (cadr action))
+		       (princ "end turn")))
+		 (print-moves (lazy-cdr moves) (1+ n)))))
+      (print-moves moves 1))
     (fresh-line)
-    (cadr (nth (1- (read)) moves))))
+    (cadr (lazy-nth (1- (read)) moves))))
 
 (defun winners (board)
   (let* ((tally (loop for hex across board
